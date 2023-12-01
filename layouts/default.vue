55749b0367d50f536b1105878f24b33ea5422235
Youssef ElAsfoury <42498703+Youssef9798@users.noreply.github.com>
2023-12-02 00:07:09 +0200

    Build default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col w-full min-h-screen">
+    <GlobalHeader />
+    <div class="flex w-full h-full">
+      <nuxt-child />
+    </div>
+  </div>
+</template>
+
+<script>
+import GlobalHeader from '~/components/global/Header.vue'
+export default {
+  components: {
+    GlobalHeader,
+  },
+}
+</script>
