44807cc8f4d050ae4997acd923d90d769bc65326
Youssef ElAsfoury <42498703+Youssef9798@users.noreply.github.com>
2023-12-02 00:06:15 +0200

    Add close icon component

---
+++ components/icons/CloseIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M12 12L7 7m5 5l5 5m-5-5l5-5m-5 5l-5 5"
+    ></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'MajesticonsClose',
+}
+</script>
